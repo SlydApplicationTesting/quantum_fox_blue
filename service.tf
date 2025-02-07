69370ca72d70b95a00be2d8ed1ccc5a212c4988e
Ksidles-Slyd <kyle.sidles@slyd.com>
2025-02-06 21:34:14 -0500

    Add service.tf

---
+++ service.tf
@@ -0,0 +1,18 @@
+resource "aws_ecs_service" "app_service" {
+  name            = "app-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "app-container"
+    container_port   = 80
+  }
+}
